f048c90048e6ce1e29da3a9d7a67b1978caada7f
Bugger <101984965+Bugger525@users.noreply.github.com>
2023-03-31 14:05:16 +0900

    Update SpriteBatch.h

--- CodeOnlyFX/include/cofx/Graphics/SpriteBatch.h
+++ CodeOnlyFX/include/cofx/Graphics/SpriteBatch.h
@@ -10,10 +10,12 @@ namespace co
 	private:
 		Shader* m_Shader;
 		unsigned int m_QuadVAO;
+		bool m_InPair;
 	public:
 		SpriteBatch();
 
 		void Begin(Shader* shader);
+		void Begin(Shader* shader, const Transform& transform);
 		void Draw(Texture* texture, const Vec2f& position, const Vec2f& size, const Color& color, float rotate);
 		void End();
 	};
